60b7751565b9fc1770b9741868a741d0dda82e61
anderrf <andersonrochafarias@gmail.com>
2019-09-30 14:35:43 -0300

    Adicao de component somar

--- src/app/somar/somar.module.ts
+++ src/app/somar/somar.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SomarComponent } from './components/somar';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SomarComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SomarComponent
   ]
 })
 export class SomarModule { }
