1befda567d37f2ee59a3438fc98e781ed9677e65
anderrf <andersonrochafarias@gmail.com>
2019-09-30 15:17:07 -0300

    Insercao de service somar

--- src/app/somar/components/somar/somar.component.ts
+++ src/app/somar/components/somar/somar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SomarService } from '../../services';
 
 @Component({
   selector: 'app-somar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SomarComponent implements OnInit {
 
-  constructor() { }
+  constructor(somarService: SomarService) { }
 
   ngOnInit() {
   }
