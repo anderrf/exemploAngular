983dc18b42dc71d800fba1b86400372c1f5a3abf
anderrf <andersonrochafarias@gmail.com>
2019-10-21 14:21:18 -0300

    somar-routing para gerenciar as rotas module somar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { SomarModule } from './somar';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { SomarModule } from './somar';
   ],
   imports: [
     BrowserModule,
-    SomarModule
+    SomarModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/somar/somar-routing.module.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { SomarComponent } from './components/somar';
+
+export const RouteSomar = [
+    { path: 'somar', component: SomarComponent }
+]
+
+/*
+    Criação do módulo para gerenciar as rotas da aplicação angular
+*/
